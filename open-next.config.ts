deabff3408a5ec2bd23acaea233b6cb756d5ea92
ParsifalC <zmw@izmw.me>
2026-05-30 19:51:30 +0800

    fix: add required middleware config and proxyExternalRequest to open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -5,6 +5,18 @@ const config: OpenNextConfig = {
     override: {
       wrapper: 'cloudflare-node',
       converter: 'edge',
+      proxyExternalRequest: 'fetch',
+      incrementalCache: 'dummy',
+      tagCache: 'dummy',
+      queue: 'dummy',
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: 'cloudflare-edge',
+      converter: 'edge',
+      proxyExternalRequest: 'fetch',
       incrementalCache: 'dummy',
       tagCache: 'dummy',
       queue: 'dummy',
